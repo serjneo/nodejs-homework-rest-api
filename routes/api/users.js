52276df21cf033e3f03ce0912cc47237d5e0795f
serjneo <sergejnechitajlenko@gmail.com>
2022-01-25 21:40:03 +0200

    added default avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 const { User } = require("../../models");
-const { authenticate } = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 const router = express.Router();
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 router.get("/logout", authenticate, async (req, res) => {
   const { _id } = req.user;
@@ -15,5 +20,29 @@ router.get("/current", authenticate, async (req, res) => {
     user: { name, email },
   });
 });
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  async (req, res) => {
+    const { path: tempUpload, filename } = req.file;
+    [extension] = filename.split(".").reverse();
+    const newFileName = `${req.user._id}.${extension}`;
+    const fileUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, fileUpload);
+    const avatarURL = path.join("avatars", newFileName);
+    try {
+      const avatarResize = await Jimp.read(fileUpload);
+      avatarResize.resize(250, 250);
+      avatarResize.write(fileUpload);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error size avatar");
+    }
+    await User.findByIdAndUpdate(req.user._id, { avatarURL }, { new: true });
+    res.json({ avatarURL });
+  }
+);
 
 module.exports = router;
