8ec7ca1de2a24559f7d082f97097996f21a5caaf
serjneo <sergejnechitajlenko@gmail.com>
2022-02-02 21:37:09 +0200

    add verification email

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,12 +3,14 @@ const { BadRequest, Conflict, Unauthorized } = require("http-errors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../../models");
 const { joiRegisterSchema, joiLoginSchema } = require("../../models/user");
+const { sendEmail } = require("../../helpers");
 
 const router = express.Router();
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, SITE_NAME } = process.env;
 
 router.post("/signup", async (req, res, next) => {
   try {
@@ -23,13 +25,21 @@ router.post("/signup", async (req, res, next) => {
     }
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
+    const verificationToken = nanoid();
     const avatarURL = gravatar.url(email);
     const newUser = await User.create({
       name,
       email,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+    const data = {
+      to: email,
+      subject: "Подтверждение email",
+      html: `<a target="blank" href="${SITE_NAME}/users/verify/${verificationToken}>Подтвердите email</a>`,
+    };
+    await sendEmail(data);
     res.status(201).json({
       name: newUser.name,
       email: newUser.email,
@@ -49,6 +59,9 @@ router.post("/login", async (req, res, next) => {
     const user = await User.findOne({ email });
     if (!user) {
       throw new Unauthorized("Email or password is wrong");
+    }
+    if (!user.verify) {
+      throw new Unauthorized("Email not verify");
     }
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { NotFound, BadRequest } = require("http-errors");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
 const { User } = require("../../models");
 const { authenticate, upload } = require("../../middlewares");
+const { sendEmail } = require("../../helpers");
+
+const { SITE_NAME } = process.env;
 const router = express.Router();
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
@@ -44,5 +48,48 @@ router.patch(
     res.json({ avatarURL });
   }
 );
+
+router.post("/verify", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      throw new BadRequest("missing required field email");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new NotFound("User not found");
+    }
+    if (user.verify) {
+      throw new BadRequest("Verification has already been passed");
+    }
+    const { verificationToken } = user;
+    const data = {
+      to: email,
+      subject: "Подтверждение email",
+      html: `<a target="blank" href="${SITE_NAME}/users/verify/${verificationToken}>Подтвердите email</a>`,
+    };
+    await sendEmail(data);
+    res.json({ message: "Verification email sent" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/verify/:verificationToken", async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+      throw new NotFound("User not found");
+    }
+    await User.findByIdAndUpdate(user._id, {
+      verificationToken: null,
+      verify: true,
+    });
+    res.json({ message: "Verification successful" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
